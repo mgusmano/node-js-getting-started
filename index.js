6e9b23f4b997226cbc9e98dd059e3cdde51c1ad9
Marc Gusmano <mfusmano@yahoo.com>
2022-03-11 13:44:49 -0500

    more

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+//index.js
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+import express from 'express';
+import cors from 'cors';
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
-  .get('/books', (req, res) => {
+var db = null 
+
+const app = express();
+//app.use(cors);
+
+// Configuring body parser middleware
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.json());
+
+
+app.get('/',(req,res) => {
+	res.send('GeeksforGeeks');
+})
+
+app.get('/books', (req, res) => {
     var books = [
         {a:1}
     ]
     res.json(books);
-  })
+});
 
+app.get('/albums', (req, res) => {
+    (async () => {
+        var rows = await db.all("select l.Title, r.Name from albums l inner join artists r on r.ArtistId = l.ArtistId")
+        res.json(rows)
+        console.log('get /albums')
+    })()
+});
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/employees', (req, res) => {
+    (async () => {
+        var rows = await db.all("select * from employees")
+        res.json(rows)
+        console.log('get /employees')
+    })()
+});
+
+const PORT = 5001;
+
+app.listen(PORT,() => {
+	console.log(`Running on PORT ${PORT}`);
+    (async () => {
+        db = await open({
+          filename: './chinook.db',
+          driver: sqlite3.Database
+        })
+    })()
+})
